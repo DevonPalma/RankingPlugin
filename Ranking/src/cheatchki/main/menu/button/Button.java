2e4cef49e7eecf3afdf388d2c14981829a4c91e4
Devon Palma <deedpa1112@gmail.com>
2018-04-17 10:16:17 -0600

    idk

--- Ranking/src/cheatchki/main/menu/button/Button.java
+++ Ranking/src/cheatchki/main/menu/button/Button.java
@@ -1,29 +1,10 @@
 package cheatchki.main.menu.button;
 
-import org.bukkit.entity.Player;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.inventory.Inventory;
-
-public abstract class Button {
+public abstract class Button implements Clickable, Drawable {
 	
-	public Button(int slot) {
-		this.slot = slot;
+	protected transient Menu menu;
+	
+	public void draw() {
+		menu.
 	}
-	
-	public Button() {}
-	
-	protected int slot;
-	
-
-	public int getSlot() {
-		return slot;
-	}
-
-	public void setSlot(int slot) {
-		this.slot = slot;
-	}
-
-	public abstract void onClick(InventoryClickEvent e);
-	
-	public abstract void draw(Inventory i, Player p);
 }
